d64441b3148fa16ce6d59e3eff73987d751e868f
Victor Hugo Lopes <hullopes@hotmail.com>
2021-01-20 21:27:11 -0300

    Add new consts

--- consts.py
+++ consts.py
@@ -5,15 +5,16 @@ MIN_REWARD = -100
 MAX_REWARD = 100
 MAX_SPECTRAL_EFF = 7.8
 K = 10
-F = [1]
+F = [2,2]
+F_D = "2-2"
 
 BLOCKS_EP = 100
 
 GAMMA = 0.7
 GAMMA_D = '07'
 #LR = 1e-03
-LR = 0.00007
-LR_D = '00007'
+LR = 0.007
+LR_D = '007'
 EPSILON = 1e-05
 EPSILON_D = '1e-05'
 
@@ -25,3 +26,5 @@ MODELS_FOLDER_STATIONARY = 'models/'
 MODELS_FOLDER = 'models_nstat/'
 
 MODELS_MMW = 'models_mmw/'
+MODELS_COLAB= 'models_colab/'
+MODELS_FINAL= 'models_final/'
