5bb2dec1d244eeed38eb0a695b27d1a23ba77dcf
Victor Hugo Lopes <hullopes@hotmail.com>
2021-01-28 14:28:12 -0300

    Add matrix converter method - transform a matrix of cell lists to a matrix of cell values

--- utils/tools.py
+++ utils/tools.py
@@ -10,3 +10,15 @@ class Tools():
         m, se = np.mean(a), scipy.stats.sem(a)
         h = se * scipy.stats.t.ppf((1 + confidence) / 2., n - 1)
         return m, m - h, m + h
+
+    @staticmethod
+    def matrix_to_vector(data):
+        '''
+        transform a matrix of cell lists to a matrix of cell values
+        :param data:
+        :return:
+        '''
+        new_data = []
+        for v in data:
+            new_data.append(np.hstack(v))
+        return new_data
