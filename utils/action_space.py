76b2d1cf8f31c029a75d9ef3a3c1c6e5c0a4180b
Victor Hugo Lopes <hullopes@hotmail.com>
2021-06-23 15:26:28 -0300

    Updating the action space generator, considering the paper' dimension equation

--- utils/action_space.py
+++ utils/action_space.py
@@ -7,6 +7,25 @@ import numpy as np
 class ActionSpace:
 
     @staticmethod
+    def get_action_space_combination(K, F):
+        '''
+        Return the action space, according to K!/((K-(len(F)*Kmax))! * (Kmax!)^len(F))
+        :param K: number of conected UEs
+        :param F: list of Kmax per frequency band
+        :return: list of possible actions act
+        '''
+        Kmax = F[0]
+        actions_c = list(combinations(list(range(0, K)), Kmax))
+        actions_p = list(permutations(actions_c, len(F)))
+        act = []
+
+        for action in actions_p:
+            if len(list(set(list(np.array(action).flatten())))) == (Kmax * len(F)):
+                act.append(action)
+        return act
+
+
+    @staticmethod
     def get_action_space(K, F):
         #actions_p = list(combinations(list(range(0, K)), np.sum(F)))
         actions_p = list(permutations(list(range(0, K)), int(np.sum(F))))
